bc9d911815a1f133f1a68d7907c163cdb1268d28
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-05-13 20:15:08 -0300

    Ex.3 MediaAritmética

--- exercises/4_2/Ex2.js
+++ exercises/4_2/Ex2.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum;
+let sum = 0;
 
-  sum = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9];
-  console.log(sum);
+for (i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i]
+}
+console.log(sum);
--- exercises/4_2/Ex3.js
+++ exercises/4_2/Ex3.js
@@ -1,5 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum;
+let sum = 0;
+let médiaAritmética;
 
-  sum = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9];
-  console.log(sum);
+for (i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
+}
+médiaAritmética = sum / numbers.length;
+console.log(médiaAritmética);
+
