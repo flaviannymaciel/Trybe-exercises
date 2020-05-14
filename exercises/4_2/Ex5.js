a3cc6b42c0ff61994426cdde5d3cc3c70e8f49c2
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-05-13 21:57:23 -0300

    Ex.7 MenorNumero

--- exercises/4_2/Ex5.js
+++ exercises/4_2/Ex5.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorValor = 0;
+let maiorValor = numbers[0];
 
 for (let i = 0; i < numbers.length; i++){
     if (numbers[i] > maiorValor){
