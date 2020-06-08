87e5b223982251bc66007c2975b5d828c685e86b
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-06-08 16:11:39 -0300

    Ex.3_ParteII

---
+++ exercises/7_2/Ex.3_parteII .js
@@ -0,0 +1,29 @@
+/* 3 - Crie uma função para mostrar o tamanho de um objeto.*/
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function objectSize(objeto) {
+  const size = Object.keys(objeto).length;
+  console.log(`Tamanho do objeto: ${size}`);
+}
+objectSize(lesson1);
+objectSize(lesson2);
+objectSize(lesson3);
