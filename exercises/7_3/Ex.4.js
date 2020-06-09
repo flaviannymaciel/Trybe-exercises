af9294a27b1ddb5d8fb19ef265cf9efceceb83cf
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-06-09 19:31:29 -0300

    Ex.5

--- exercises/7_3/Ex.4.js
+++ exercises/7_3/Ex.4.js
@@ -21,6 +21,6 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
-
+assert.deepStrictEqual(myRemove([1, 2, 3, 4]), [1, 2, 3, 4]);
 
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
