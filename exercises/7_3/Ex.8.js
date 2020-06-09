f672a929aa8585db0b2509bf4b5b5488e3399569
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-06-09 20:05:46 -0300

    Ex.8

---
+++ exercises/7_3/Ex.8.js
@@ -0,0 +1,8 @@
+/* 7- este se uma função foi definida*/
+
+const assert = require('assert');
+
+function thereIs() {}
+
+// implemente seus testes aqui
+assert.strictEqual(typeof thereIs, 'function');
