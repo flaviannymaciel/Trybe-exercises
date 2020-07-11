6d22fddd4cdde55d865f651ca79ed726da088642
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-11 19:40:42 -0300

    my-pokedex

---
+++ exercises/front-end/11_2/my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import data from './data.js';
+import './App.css';
+import Pokedex from './Pokedex.js';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className= 'title'>My Pokedex</h1>
+      <Pokedex pokemons={data}/>
+    </div>
+  );
+}
+
+export default App;
