066b90e7b2263fd8e27b517a480dbc20fee0adc6
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-31 16:14:34 -0300

    prosp.children-aula13.1

--- exercises/front-end/13_1/cofre/src/App.js
+++ exercises/front-end/13_1/cofre/src/App.js
@@ -5,7 +5,7 @@ import Cofre from './Cofre'
 function App() {
   return (
     <div className="App">
-      <Cofre>Tesouro</Cofre>
+      <Cofre userPassword="1234"><img src="https://www.pmcofres.com.br/wp-content/uploads/2015/02/15_26_07_992_file1.jpg" alt="cofre"/></Cofre>
     </div>
   );
 }
--- exercises/front-end/13_1/cofre/src/Cofre.jsx
+++ exercises/front-end/13_1/cofre/src/Cofre.jsx
@@ -3,21 +3,52 @@ import './Cofre.css'
 
 //props - userPassoword -> 12345
 class Cofre extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      givenPassword: '', //quando voce precisa do estado atual para retornar um novo o react recomenta passar uma callback para o setStage e não um objeto direto como no exercício de forms.
+      correctPassword: false
+    }
+    this.checkPassword = this.checkPassword.bind(this)
+  }
+
+  inputDigits = (digit) => {
+    this.setState((currentState) => ({
+      givenPassword: `${currentState.givenPassword}${digit}`
+    }))
+  }
+
+  checkPassword = () => {
+    if (this.state.givenPassword === this.props.userPassword)
+      this.setState({correctPassord: true})
+    else {
+      alert("Senha errada");
+      this.setState({correctPassord: false, givenPassword: ""})
+    }
+    
+  }
   render() {
     console.log('componente recebento props', this.props);
     const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    if (this.state.correctPassword) return <div>(this.props.children)</div>
     return (
       <div className="safe">
         <div className="keyboard">
-          <p>Empty Passoword</p>
+          <p className="keyboard-element current-password">{this.state.givenPassword || 'Empty Password'}</p>
           {digits.map((digit) => (
             <button
               type="button"
               className="keyboard-button"
+              onClick={() => this.inputDigits(digit)}
               key={digit} // precisa usar por gerar dinamicamente, digit por ser único
             >{digit}
             </button>
           ))}
+          <button
+            type="button"
+            className="keyboard-element"
+            onClick={this.checkPassword}            
+          >Inserir senha</button>
         </div>
       </div>
     );
