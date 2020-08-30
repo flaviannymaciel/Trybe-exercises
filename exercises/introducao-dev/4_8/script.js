03f7c8f3bfefce22e23e2e9f4cbb774228fdcbbd
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-08-30 16:32:53 -0300

    criando JavaScript

---
+++ exercises/introducao-dev/4_8/script.js
@@ -0,0 +1,39 @@
+const formulario = document.getElementById('control');
+const button = document.querySelector('.btn');
+const text = document.querySelector('.css');
+
+formulario.addEventListener('change', handleChange);
+
+const handleStyle = {
+  bg(value) {
+    button.style.backgroundColor = value;
+  },
+  color(value) {
+    button.style.color = value;
+  },
+  fontSize(value) {
+    button.style.fontSize = value + 'px';
+  },
+  fontFamily(value) {
+    button.style.fontFamily = value;
+  },
+  text(value) {
+    button.innerText = value;
+  },
+  space(value) {
+    button.style.lineHeight = value + 'px';
+  },
+};
+
+function handleChange (event) {
+  const name = event.target.name;
+  const value = event.target.value;
+  console.log(handleStyle);
+  
+  handleStyle[name](value);
+  showCss()
+};
+
+function showCss() {
+  text.innerHTML = button.style.cssText
+}
