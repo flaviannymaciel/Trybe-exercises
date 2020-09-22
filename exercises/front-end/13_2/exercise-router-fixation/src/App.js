be3304bb1e9b89d6609c026ded568fb9baf8f346
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-22 19:23:44 -0300

    exerc 7ao9

--- exercises/front-end/13_2/exercise-router-fixation/src/App.js
+++ exercises/front-end/13_2/exercise-router-fixation/src/App.js
@@ -9,11 +9,15 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Link to="/" > Home </Link>
-        <Link to="/about" > About </Link>
-        <Link to="/users" > Users </Link>
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/users"> Users </Link>
         <Route exact path="/about" component={About} />
-        <Route exact path="/users" component={Users} />
+        <Route
+          exact
+          path="/users/:userId"
+          render={() => <Users greetingMessage="Good Moorning" />}
+        />
         <Route exact path="/" component={Home} />
       </BrowserRouter>
     );
