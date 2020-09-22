8e54839a63f4a24e4668acc5dd66ac0dade99316
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-22 19:03:05 -0300

    exerc 1ao6

--- exercises/front-end/13_2/exercise-router-fixation/src/App.js
+++ exercises/front-end/13_2/exercise-router-fixation/src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Home from './Home';
 import { BrowserRouter, Route } from 'react-router-dom';
 import About from './About';
+import Users from './Users';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/" > Home </Link>
+        <Link to="/about" > About </Link>
+        <Link to="/users" > Users </Link>
         <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
         <Route exact path="/" component={Home} />
       </BrowserRouter>
     );
