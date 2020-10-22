b7e6f5df0a868cfa4925eaf4c5bd1d8e762c41cf
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-10-22 15:32:42 -0300

    exercise1

---
+++ exercises/front-end/16_1/action-creator.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/front-end-libraries/redux/define-an-action-creator
+// Redux: Define an Action Creator
+// Define a function named actionCreator() that returns the action object when called.
+
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+function actionCreator() {
+  return action;
+}
