d026ce6de4cd65eba21573b144f9252296a22f57
flaviannymaciel <flaviannymaciel@gmail.com>
2021-01-04 20:08:06 -0300

    rename folder

--- exercises/back-end/sql/21.1/exercicios.sql
+++ exercises/back-end/sql/21.1/exercicios.sql
@@ -1,3 +1,4 @@
+-- Exercise 1
 -- Find the longest time that an employee has been at the studio
 SELECT MAX(years_employed) as Max_years_employed
 FROM employees;
@@ -9,6 +10,8 @@ GROUP BY role;
 SELECT role, AVG(years_employed) as Average_years_employed
 FROM employees
 GROUP BY role;
+
+-- Exercise 2
 -- Find the number of Artists in the studio (without a HAVING clause)
 SELECT role, COUNT(*) as Number_of_artists
 FROM employees
@@ -22,3 +25,17 @@ SELECT role, SUM(years_employed)
 FROM employees
 GROUP BY role
 HAVING role = "Engineer";
+
+-- Exercise 3
+-- Write a query to list the number of jobs available in the employees table.
+SELECT COUNT(DISTINCT job_id) 
+FROM employees;
+-- Write a query to get the total salaries payable to employees.
+SELECT SUM(salary) 
+FROM employees;
+-- Write a query to get the maximum salary of an employee working as a Programmer. 
+SELECT MAX(salary) 
+FROM employees 
+WHERE job_id = 'IT_PROG';
+
+
